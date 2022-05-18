018b327067d00bdc6670de5ff6cc8965ed2fbf97
Taiju Sanagi <taijusanagi@gmail.com>
2022-05-19 01:12:30 +0900

    chore: implement proxy pattern

--- packages/contracts/tasks/wrap-deploy-target.js
+++ packages/contracts/tasks/wrap-deploy-target.js
@@ -2,9 +2,10 @@ task("wrap-deploy-target", "deploy wrap target xNFTs contract")
   .addParam("selfDomain", "self domain")
   .addParam("connext", "connext")
   .addParam("dummyTransactingAssetId", "dummy transacting asset id")
-  .setAction(async ({ selfDomain, connext, dummyTransactingAssetId }) => {
+  .addParam("nftImplementation", "nft implementation")
+  .setAction(async ({ selfDomain, connext, dummyTransactingAssetId, nftImplementation }) => {
     const XNFTTarget = await ethers.getContractFactory("xNFTTarget");
-    const xNFTTarget = await XNFTTarget.deploy(selfDomain, connext, dummyTransactingAssetId);
+    const xNFTTarget = await XNFTTarget.deploy(selfDomain, connext, dummyTransactingAssetId, nftImplementation);
     await xNFTTarget.deployed();
     console.log("Deployed to: ", xNFTTarget.address);
   });
