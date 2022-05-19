d2bed7f050e96ed5399c82305b9a2ed8622c185d
kenta suhara <ponta.blockchain@gmail.com>
2022-05-19 22:47:52 +0900

    fix: shadow

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import { Box, Flex, Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import { Box, Flex, Tabs, TabList, TabPanels, Tab, TabPanel, useColorModeValue } from "@chakra-ui/react";
 import { Native } from "../component/Native";
 import { Wrap } from "../component/Wrap";
 import { Layout } from "../component/Layout";
@@ -8,7 +8,15 @@ const IndexPage: NextPage = () => {
   return (
     <Layout>
       <Flex justifyContent="center">
-        <Box width="xl" border="1px" borderColor="gray.300" paddingY={10} paddingX={4} rounded="2xl" boxShadow={"xl"}>
+        <Box
+          width="xl"
+          border="1px"
+          borderColor={useColorModeValue("gray.300", "gray.500")}
+          paddingY={10}
+          paddingX={4}
+          rounded="2xl"
+          boxShadow={"xl"}
+        >
           <Tabs>
             <TabList>
               <Tab>Wrap</Tab>
