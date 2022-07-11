28678d737508c05d3f1e4a6d1715bcfa59bb3915
just-a-node <eye1717@gmail.com>
2022-07-11 16:30:00 -0600

    build: update connext version

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -28,7 +28,12 @@ dotenv.config();
 const accounts = process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [];
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.11",
+  solidity: {
+    compilers: [
+      {version: "0.8.11"},
+      {version: "0.8.15"}
+    ]
+  },
   networks: {
     hardhat: process.env.FORK_RINKEBY
       ? {
