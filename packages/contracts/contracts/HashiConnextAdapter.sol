65d6e86c9301ee7b4b50bc0b272310eccb88aae2
aqrare <takumitakumi0310@gmail.com>
2022-07-20 14:31:43 +0900

    fix: event

--- packages/contracts/contracts/HashiConnextAdapter.sol
+++ packages/contracts/contracts/HashiConnextAdapter.sol
@@ -19,7 +19,7 @@ contract HashiConnextAdapter is OwnableUpgradeable, ERC165Upgradeable {
   uint32 private _selfDomain;
 
   event BridgeSet(uint32 domain, address bridgeContract);
-  event ConnextSet(address connextContract, address executorContract);
+  event ConnextSet(address connextContract);
   event SelfDomainSet(uint32 selfDomain);
   event TransactingAssetIdSet(address transactingAssetId);
 
@@ -40,7 +40,7 @@ contract HashiConnextAdapter is OwnableUpgradeable, ERC165Upgradeable {
   function setConnext(address connextContract) public onlyOwner {
     _connext = connextContract;
     _executor = address(IConnextHandler(_connext).executor());
-    emit ConnextSet(connextContract, _executor);
+    emit ConnextSet(connextContract);
   }
 
   function setSelfDomain(uint32 selfDomain) public onlyOwner {
