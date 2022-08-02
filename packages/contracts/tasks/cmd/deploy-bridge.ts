afe332e4324791ce3d4d9f6927521a98d97c86dd
just-a-node <eye1717@gmail.com>
2022-08-01 23:33:13 -0600

    chore: zero address for asset, tasks changes

--- packages/contracts/tasks/cmd/deploy-bridge.ts
+++ packages/contracts/tasks/cmd/deploy-bridge.ts
@@ -3,15 +3,13 @@ import { task } from "hardhat/config";
 task("cmd-deploy-bridge", "cmd deploy bridge")
   .addParam("selfDomain", "self domain")
   .addParam("connext", "connext")
-  .addParam("dummyTransactingAssetId", "dummy transacting asset id")
   .addParam("wrappedNftImplementation", "nft implementation")
   .setAction(
-    async ({ selfDomain, connext, dummyTransactingAssetId, wrappedNftImplementation }, { ethers, upgrades }) => {
+    async ({ selfDomain, connext, wrappedNftImplementation }, { ethers, upgrades }) => {
       const Hashi721Bridge = await ethers.getContractFactory("Hashi721Bridge");
       const hashi721Bridge = await upgrades.deployProxy(Hashi721Bridge, [
         selfDomain,
         connext,
-        dummyTransactingAssetId,
         wrappedNftImplementation,
       ]);
       await hashi721Bridge.deployed();
