f4c4c4f15f75b53c7fa5f8ad4df1847832f96d49
taijusanagi <taijusanagi@gmail.com>
2023-04-06 12:21:55 +0900

    fix: update integration test script

--- packages/contracts/test/integration.test.ts
+++ packages/contracts/test/integration.test.ts
@@ -1,6 +1,7 @@
 import { expect } from "chai";
 import { ethers, network } from "hardhat";
 
+import { TOKEN_URI_LENGTH_LIMIT } from "../config";
 import networkJsonFile from "../network.json";
 import { isChainId } from "../types/ChainId";
 import { ADDRESS_1, ADDRESS_2 } from "./helper/constant";
@@ -35,14 +36,13 @@ onlyForIntegrationTest("Integration Test", function () {
         await wrappedHashi721.connect(owner).initialize();
         const connext = selfNetwork.deployments.connext;
         const hashi721Bridge = await Hashi721Bridge.deploy();
-        await hashi721Bridge.connect(owner).initialize(connext, wrappedHashi721.address);
+        await hashi721Bridge.connect(owner).initialize(connext, wrappedHashi721.address, TOKEN_URI_LENGTH_LIMIT);
         const mintedTokenId = 1;
         const tokenURI = "";
         await wrappedHashi721.connect(owner).mint(holder.address, mintedTokenId, tokenURI);
         await wrappedHashi721.connect(holder).setApprovalForAll(hashi721Bridge.address, true);
         const destination = targetNetwork.domainId;
         const relayerFee = 0;
-        const slippage = 0;
         const asset = wrappedHashi721.address;
         const to = ADDRESS_2;
         const tokenId = mintedTokenId;
@@ -50,7 +50,7 @@ onlyForIntegrationTest("Integration Test", function () {
         const bridge = ADDRESS_1;
         await hashi721Bridge.connect(owner).setBridge(destination, bridge);
         await expect(
-          hashi721Bridge.connect(holder).xCall(destination, relayerFee, slippage, asset, to, tokenId, isTokenURIIgnored)
+          hashi721Bridge.connect(holder).xCall(destination, relayerFee, asset, to, tokenId, isTokenURIIgnored)
         )
           .to.emit(wrappedHashi721, "Transfer")
           .withArgs(holder.address, hashi721Bridge.address, tokenId);
